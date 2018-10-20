def1b576229c0bab70971ead65db740c075a50bd
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-10-20 10:27:40 -0700

    break comment across lines

--- r-gis-code/install_spatial_packages.R
+++ r-gis-code/install_spatial_packages.R
@@ -1,6 +1,9 @@
 #! /usr/bin/env Rscript
 
-# Note: I run this on (Arch) Linux, So it installs from source, and there are quite a few Linux dependencies that need to be installed. If you run this on Windows or MacOS, it *should* install binary packages with all rhe dependencies bundled in.
+# Note: I run this on (Arch) Linux, So it installs from source, and there
+# are quite a few Linux dependencies that need to be installed. If you run
+# this on Windows or MacOS, it *should* install binary packages with all
+# the dependencies bundled in.
 install.packages(c(
   "tidyverse",
   "sf",
