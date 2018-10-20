cde741491afc13e2d40a489415cbd57d9b6581d7
Daniel Nguyen <daniel.nguyen@intel.com>
2018-10-19 19:36:51 -0700

    Deploys webapp on Heroku

--- webapp/app.py
+++ webapp/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -8,4 +9,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
