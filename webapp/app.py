116beb56b6586169906c930840cf34005e41ed92
Daniel Nguyen <daniel.nguyen@intel.com>
2018-10-19 18:54:32 -0700

    Fixes Dockerfile

--- webapp/app.py
+++ webapp/app.py
@@ -7,20 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/on")
-def lights_on():
-    with open("./lights", "w") as f:
-        f.write("1")
-    return "1"
-
-@app.route("/off")
-def lights_off():
-    with open("./lights", "w") as f:
-        f.write("0")
-    return "0"
-
-@app.route("/lights")
-def get_lights():
-    with open("./lights", "r") as f:
-        light_status = f.read()
-    return light_status
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000, debug=True)
--- webapp/main.py
+++
@@ -1,4 +0,0 @@
-from pythonserver import app as application
-
-if __name__ == "__main__":
-    application.run(debug=False)
